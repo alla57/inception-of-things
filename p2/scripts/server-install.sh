ff686026e2efea8216da2d683570bfc7e8aca71a
alla57 <a.boumediene01@gmail.com>
2024-05-22 12:52:53 +0200

    new dep test p2

---
+++ p2/scripts/server-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Disabling firewall
+ufw disable
+
+# Install and configure k3s server
+curl -sfL https://get.k3s.io | sh -s - --flannel-iface enp0s8 --node-ip="192.168.56.110"
+
+cd /vagrant/confs/
+
+cp dep1.yml ingress.yml /home/vagrant
+
+cd /home/vagrant
+
+sudo kubectl apply -f .
